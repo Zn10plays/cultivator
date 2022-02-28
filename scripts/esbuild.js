35ce60eeb1150eb590937c1acb2ea9d5d0ac97e9
Rudra Patel <rudrapatel26122004@gmail.com>
2022-02-28 17:49:29 -0500

    added a dev build command with esbuild

---
+++ scripts/esbuild.js
@@ -0,0 +1,25 @@
+const esbuild = require('esbuild');
+const fs = require('fs')
+const path = require('path')
+
+async function buildDir(start) {
+  const relativePath = start ? start : './src'
+
+  const all = await fs.readdirSync(relativePath)
+  const files = all.filter(ext => ext.endsWith('.ts'))
+    .map(name => path.resolve(relativePath, name))
+
+  const dirs = all.filter(ext => !ext.endsWith('.ts'))
+
+  await esbuild.buildSync({
+    entryPoints: files,
+    outdir: path.join('./build', relativePath.replace('src', ''))
+  })
+
+  for (let i = 0; i < dirs.length; i++) {
+    console.log('building', dirs[i], relativePath)
+    buildDir(path.join(relativePath, dirs[i]));
+  }
+}
+
+buildDir()
