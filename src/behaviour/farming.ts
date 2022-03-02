d6ade6399548f6a35e991838f8134459e17eb6f0
Rudra Patel <rudrapatel26122004@gmail.com>
2022-03-01 19:58:26 -0500

    more code

---
+++ src/behaviour/farming.ts
@@ -0,0 +1,8 @@
+import { BehaviorEquipItem, BehaviorMoveTo } from 'mineflayer-statemachine'
+import { Bot } from 'mineflayer'
+
+function FarmingBehavior(bot: Bot, target) {
+
+}
+
+export default FarmingBehavior
--- src/behaviour/innitStates.ts
+++ src/behaviour/innitStates.ts
@@ -1,6 +1,13 @@
+import { NestedStateMachine } from "mineflayer-statemachine"
+import FarmingBehavior from "./farming"
+import { Bot } from "mineflayer"
+import data from "minecraft-data";
 
-function initState(bot) {
+function initState(bot: Bot) {
+  const mcData = data(bot.version);
   const target = {}
+
+  const farm = FarmingBehavior(bot, target)
 }
 
 export default initState
